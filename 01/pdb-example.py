1601e32b728591c5af30d83a7da2dd015c51fd4c
Erick Matsen <ematsen@gmail.com>
2019-12-26 07:38:06 -0800

    better pdb example

--- 01/pdb-example.py
+++ 01/pdb-example.py
@@ -1,10 +1,23 @@
 import torch
+import torch.optim as optim
 
-x = torch.eye(3)
+
+def f(in_x, target):
+    return (in_x - target).pow(2)
+
+
+x = torch.tensor(5.0, requires_grad=True)
+target = torch.tensor(1.0)
+
+# lr stands for learning rate, i.e. stepsize.
+optimizer = optim.Adam([x], lr=1.0)
 
 breakpoint()
 
-for _ in range(5):
-    x += 1
+for step_idx in range(250):
+    optimizer.zero_grad()
+    y = f(x, target)
+    y.backward()
+    optimizer.step()
 
 print(x)
